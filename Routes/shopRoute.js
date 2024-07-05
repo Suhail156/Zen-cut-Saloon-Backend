81f3df4b37a1283b2208856bea9ec9930e4d4440
muhammad suhail <suhailpk539@gmail.com>
2024-07-05 20:29:01 +0530

    .

--- Routes/shopRoute.js
+++ Routes/shopRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { addShop } from '../Controller/shopController.js'
+import imageUpload from '../Middlewares/ImageUpload.js/ImageUpload.js'
 
  const router=express.Router()
 
- router.post('/shops',addShop)
+ router.post('/shops',imageUpload,addShop)
 
  export default router
