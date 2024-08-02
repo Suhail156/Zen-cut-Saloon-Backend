1f384b9a746cd4da48319070c0886959bab2cd1b
muhammad suhail <suhailpk539@gmail.com>
2024-08-02 21:57:23 +0530

    .

--- Models/shopSchema.js
+++ Models/shopSchema.js
@@ -1,54 +1,57 @@
 import mongoose from "mongoose";
 
 const shopSchema = new mongoose.Schema({
-    ownerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "shopOwner",
-        required: true
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "shopOwner",
+    required: true,
+  },
+  shopname: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  category: [
+    {
+      type: String,
+      required: true,
     },
-    shopname: {
-        type: String,
-        required: true
+  ],
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  startTime: {
+    type: String,
+    // required:true
+  },
+  endTime: {
+    type: String,
+    // required:true
+  },
+  booking: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Booking",
     },
-    phone: {
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },  
-    location: {
-        type: String,
-        required: true
-    },
-    category: [{
-        type: String,
-        required: true 
-    }],
-    isDeleted: {    
-        type: Boolean,
-        default: false
-    },
-    startTime:{
-        type:String,
-        // required:true
-    },
-    endTime:{
-        type:String,
-        // required:true
-    },
-    booking:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Booking"
-    }],
-
+  ],
 });
 
 const Shop = mongoose.model("Shop", shopSchema);
 
-export default Shop;    
+export default Shop;
